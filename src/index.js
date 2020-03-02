7afde14b94151583cf7126cf3c07a53284385c38
ehsan69h <e.haghani@outlook.com>
2020-03-02 20:29:15 +0330

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App timeTillDate="05 26 2019, 6:00 am" timeFormat="MM DD YYYY, h:mm a"/>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
